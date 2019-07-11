f13a8992038689420add9f194422af54f1d70840
happydudu <happydudu1@gmail.com>
2019-07-10 20:27:05 -0700

    az files

---
+++ src/main/java/com/svit/ecommerce/HelloController.java
@@ -0,0 +1,19 @@
+package com.svit.ecommerce;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World, Spring!";
+    }
+    
+    @RequestMapping("/abc/")
+    public String indexAbc() {
+        return "Hello World, Spring abc!";
+    }
+    
+}
